ad0d3dfebe9f3ad65378ccb25e44938c84357593
regbo <reggie.pierce@gmail.com>
2026-02-04 16:05:20 -0500

    Update config.py

--- src/lfp_logging/config.py
+++ src/lfp_logging/config.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 
 from lfp_logging import log_level
 from lfp_logging.log_level import LogLevel
@@ -15,8 +14,6 @@ LOG_LEVEL_DEFAULT = logging.INFO
 LOG_FORMAT_DATE_DEFAULT = "%Y-%m-%d %H:%M:%S"
 LOG_FORMAT_DEFAULT = "%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s"
 
-LOG_LEVEL_SYS_ARG_NAME = "--log-level"
-
 LOG_LEVEL_ENV_NAME = "LOG_LEVEL"
 LOG_FORMAT_DATE_ENV_NAME = "LOG_FORMAT_DATE"
 LOG_FORMAT_STDOUT_ENV_NAME = "LOG_FORMAT_STDOUT"
@@ -25,14 +22,9 @@ LOG_FORMAT_STDERR_ENV_NAME = "LOG_FORMAT_STDERR"
 
 def level() -> LogLevel:
     """
-    Retrieves the global log level from system arguments or environment variables.
-    System arguments take precedence over environment variables.
+    Retrieves the global log level from environment variables.
     """
     value = os.environ.get(LOG_LEVEL_ENV_NAME, None)
-    for idx in range(1, len(sys.argv)):
-        arg = sys.argv[idx]
-        if arg == LOG_LEVEL_SYS_ARG_NAME:
-            value = sys.argv[idx + 1] if idx < len(sys.argv) - 1 else None
     return log_level.get(value, LOG_LEVEL_DEFAULT)
 
 
